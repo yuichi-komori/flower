e40e20800c34829c024d5c42a7a33ae56d6fc733
yoneyama <r.yoneyama@geexjp.com>
2021-09-04 21:38:14 +0900

    main-sceneの実装_2 ⇒以下の部分を実装しました。 　・敵の設定(個々randomに動きます) 　・プレイヤーと敵の衝突アクションを追加

--- scenes/main-scene.ts
+++ scenes/main-scene.ts
@@ -9,6 +9,13 @@ const MAIN_HEIGHT = 600;         // メイン画面のheight
 const WORLD_RECORD_SPEED = 5.67; // マラソンの男子世界記録[m/s]
 const PLAYER_SPEED_X = 160;      // プレイヤーの速度(x) [p/s]
 const PLAYER_SPEED_Y = 330;      // プレイヤーの速度(y) [p/s]
+const ENEMY_SPEED_X = 160;       // 敵の速度(x) [p/s]
+const ENEMY_SPEED_Y = 330;       // 敵の速度(y) [p/s]
+const ENEMY_STEP_X = 500;        // 敵の間隔(x)
+const ENEMY_BY_FRAME = 20;       // 敵の挙動不審具合[f] (低い：よく動く, 高い：あまり動かない)
+const HIT_SPEED_X = 160;         // 衝突時の速度(x) [p/s]
+const HIT_SPEED_Y = 200;         // 衝突時の速度(y) [p/s]
+
 
 // ゲームオブジェクト
 let timer;
@@ -16,7 +23,10 @@ let bgm;
 let soundGoal;
 let soundRun;
 let soundJump;
+let soundDamaging;
 let player;
+let playerDamaging = false;  // プレイヤーのState(ダメージ状態)
+let enemies;
 let coins;
 let bombs;
 let goals;
@@ -27,6 +37,8 @@ let gameOver = false;
 let timeText;
 let scoreText;
 let clearText;
+let updateByFrame = 0;        // ※Update()で使用
+
 
 export default class MainScene extends Phaser.Scene {
   private acorn: Phaser.Physics.Arcade.Image;
@@ -45,6 +57,7 @@ export default class MainScene extends Phaser.Scene {
     this.load.audio('soundGoal', ['../assets/sound/main_goal.mp3']); // ゴール音
     this.load.audio('soundRun', ['../assets/sound/main_run.mp3']); // 走る音
     this.load.audio('soundJump', ['../assets/sound/main_jump.mp3']); // ジャンプ音
+    this.load.audio('soundDamaging', ['../assets/sound/main_damaging.mp3']); // ダメージ音
     this.load.image('sky', '../assets/main_background.png'); // 背景
     this.load.image('ground', '../assets/main_ground.png');  // 地面
     this.load.image('block', '../assets/main_block.png');   // 壁
@@ -53,6 +66,7 @@ export default class MainScene extends Phaser.Scene {
     this.load.image('goal', '../assets/main_goal.png');      // ゴール
     this.load.image('backButton', '../assets/back.png');      // 戻るボタン
     this.load.spritesheet('player', 'assets/soldier_60x95.png', { frameWidth: 60, frameHeight: 95 }); // プレイヤー
+    this.load.spritesheet('enemy', 'assets/enemy_65x81.png', { frameWidth: 65, frameHeight: 81 }); // 敵
   }
 
   // 初期化
@@ -77,6 +91,7 @@ export default class MainScene extends Phaser.Scene {
     soundGoal = this.sound.add('soundGoal',{ volume: 1 });
     soundRun = this.sound.add('soundRun',{ volume: 0.5, loop: true });
     soundJump = this.sound.add('soundJump',{ volume: 0.5 });
+    soundDamaging = this.sound.add('soundDamaging', {volume: 1});
 
     // 背景の設定
     this.autoFill(0, 0, 800, 600, function(scene, x, y){
@@ -108,35 +123,88 @@ export default class MainScene extends Phaser.Scene {
     this.cameras.main.startFollow(player, true);
 
     this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'player', frame: 0 }],
-        frameRate: 20
-    });
-
-    this.anims.create({
-      key: 'up-right',
-      frames: this.anims.generateFrameNumbers('player', { start: 1, end: 4 }),
+      key: 'damaging',
+      frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
       frameRate: 3,
       repeat: 1
     });
 
     this.anims.create({
-      key: 'up-left',
+        key: 'turn',
+        frames: [{ key: 'player', frame: 4 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'up-right',
       frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
       frameRate: 3,
       repeat: 1
     });
 
     this.anims.create({
+      key: 'up-left',
+      frames: this.anims.generateFrameNumbers('player', { start: 9, end: 12 }),
+      frameRate: 3,
+      repeat: 1
+    });
+
+    this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('player', { start: 9, end: 16 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 13, end: 20 }),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
       key: 'left',
-      frames: this.anims.generateFrameNumbers('player', { start: 17, end: 24 }),
+      frames: this.anims.generateFrameNumbers('player', { start: 21, end: 28 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    // 敵の設定
+    enemies = this.physics.add.group({
+          key: 'enemy',
+          repeat: Math.floor(GOAL_X / ENEMY_STEP_X),
+          setXY: { x: ENEMY_STEP_X, y: 0, stepX: ENEMY_STEP_X }
+    });
+    
+    enemies.children.iterate(function (enemy) {
+          enemy.setBounce(0.2);
+          enemy.setCollideWorldBounds(true);
+    });
+    
+    this.anims.create({
+      key: 'enemy-turn',
+      frames: [{ key: 'enemy', frame: 0 }],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'enemy-up-right',
+      frames: this.anims.generateFrameNumbers('enemy', { start: 1, end: 2 }),
+      frameRate: 3,
+      repeat: 1
+    });
+
+    this.anims.create({
+      key: 'enemy-up-left',
+      frames: this.anims.generateFrameNumbers('enemy', { start: 3, end: 4 }),
+      frameRate: 3,
+      repeat: 1
+    });
+
+    this.anims.create({
+      key: 'enemy-right',
+      frames: this.anims.generateFrameNumbers('enemy', { start: 5, end: 12 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'enemy-left',
+      frames: this.anims.generateFrameNumbers('enemy', { start: 13, end: 20 }),
       frameRate: 10,
       repeat: -1
     });
@@ -168,76 +236,90 @@ export default class MainScene extends Phaser.Scene {
     scoreText = this.add.text(16, 48, 'score: 0', { fontSize: '32px'});
     scoreText.setScrollFactor(0);
 
-    // 衝突判定の設定。地面は、コイン・プレイヤー・爆弾と衝突する。これがないと地面を貫通してしまう
+    // 衝突判定の設定。地面は、コイン・プレイヤー・爆弾・敵と衝突する。これがないと地面を貫通してしまう
     this.physics.add.collider(player, platforms);
+    this.physics.add.collider(enemies, platforms);
     // this.physics.add.collider(coins, platforms);
     // this.physics.add.collider(bombs, platforms);
 
     // プレイヤーとコインが重なれば、collectCoin() を呼び出す
     // this.physics.add.overlap(player, coins, this.collectCoin, null, this);
     // this.physics.add.collider(player, bombs, this.hitBomb, null, this);
+    this.physics.add.overlap(player, enemies, this.hitEnemy, null, this);
     this.physics.add.overlap(player, goals, this.gameClear, null, this);
   }
   
   // ゲーム進行中に呼び出す
   update() {
-      // ゲームオーバーになったら処理終了(クリア時もここに入る)
+    // ゲームオーバーになったら処理終了(クリア時もここに入る)
     if (gameOver) {
       return;
     }
 
-    // ←キー押下
-    if (cursors.left.isDown) {
-      player.setVelocityX(-PLAYER_SPEED_X);
+    // プレイヤーを更新
+    if(playerDamaging){
+      player.anims.play('damaging', true);
+      // 1秒待機
+      setTimeout(() => {
+        player.setTint(0xffffff);
+        playerDamaging = false;
+      }, 1000);
+    }
+    else{
+      // ←キー押下
+      if (cursors.left.isDown) {
+        player.setVelocityX(-PLAYER_SPEED_X);
 
-      if(!player.body.touching.down){
-        player.anims.play('up-left', true);
-      }
-      else{
-        player.anims.play('left', true);
+        if(!player.body.touching.down){
+          player.anims.play('up-left', true);
+        }
+        else{
+          player.anims.play('left', true);
 
-        if(!soundRun.isPlaying){
-          soundRun.play();
+          if(!soundRun.isPlaying){
+            soundRun.play();
+          }
         }
       }
-    }
 
-    // →キー押下
-    else if (cursors.right.isDown) {
-      player.setVelocityX(PLAYER_SPEED_X);
+      // →キー押下
+      else if (cursors.right.isDown) {
+        player.setVelocityX(PLAYER_SPEED_X);
 
-      if(!player.body.touching.down){
-        player.anims.play('up-right', true);
-      }
-      else{
-        player.anims.play('right', true);
+        if(!player.body.touching.down){
+          player.anims.play('up-right', true);
+        }
+        else{
+          player.anims.play('right', true);
 
-        if(!soundRun.isPlaying){
-          soundRun.play();
+          if(!soundRun.isPlaying){
+            soundRun.play();
+          }
         }
       }
-    }
 
-    else {
-      player.setVelocityX(0);
+      else {
+        player.setVelocityX(0);
 
-      soundRun.stop();
+        soundRun.stop();
 
-      if(!player.body.touching.down){ 
-        player.anims.play('up-right', true);
+        if(!player.body.touching.down){ 
+          player.anims.play('up-right', true);
+        }
+        else{
+          player.anims.play('turn');
+        }
       }
-      else{
-        player.anims.play('turn');
+
+      // ↑キー押下
+      if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-PLAYER_SPEED_Y);
+
+        soundRun.stop();
+        soundJump.play();
       }
     }
-
-    // ↑キー押下
-    if (cursors.up.isDown && player.body.touching.down) {
-      player.setVelocityY(-PLAYER_SPEED_Y);
-
-      soundRun.stop();
-      soundJump.play();
-    }
+    
   
     // プレイ時間を設定
     let elapsedSeconds = timer.getElapsedSeconds();
@@ -249,6 +331,51 @@ export default class MainScene extends Phaser.Scene {
     score = this.convertXToMeter(player.body.position.x);
     let score_km = Math.floor(score / 1000);
     scoreText.setText('Score: ' + ((score_km === 0) ? '' : score_km + "km ") + (score % 1000) + 'm');
+
+    // 敵を更新
+    updateByFrame++;
+    if(updateByFrame == ENEMY_BY_FRAME){
+      enemies.children.iterate(function (enemy){
+        let random_rate = Math.random();
+        if(random_rate < 0.3){
+          enemy.setVelocityX(-ENEMY_SPEED_X);
+
+          if(!enemy.body.touching.down){
+            enemy.anims.play('enemy-up-left', true);
+          }
+          else{
+            enemy.anims.play('enemy-left', true);
+          }
+        }
+        else if(random_rate < 0.6){
+          enemy.setVelocityX(ENEMY_SPEED_X);
+
+          if(!enemy.body.touching.down){
+            enemy.anims.play('enemy-up-right', true);
+          }
+          else{
+            enemy.anims.play('enemy-right', true);
+          }
+        }
+        else if(random_rate < 0.9){
+          enemy.setVelocityX(0);
+
+          if(!enemy.body.touching.down){
+            enemy.anims.play('enemy-up-right', true);
+          }
+          else{
+            enemy.anims.play('enemy-turn');
+          }
+        }
+        else{
+          if(enemy.body.touching.down){
+            enemy.setVelocityY(-ENEMY_SPEED_Y);
+          }
+        }
+      });
+
+      updateByFrame = 0;
+    }
   }
   
   // // コインと接触した時
@@ -285,6 +412,15 @@ export default class MainScene extends Phaser.Scene {
   //   player.anims.play('turn');
   //   gameOver = true;
   // }
+
+  // 敵と接触したとき
+  hitEnemy(player, enemy){
+    soundDamaging.play();
+    player.setTint(0xff0000);
+    player.setVelocityX(-HIT_SPEED_X);
+    player.setVelocityY(-HIT_SPEED_Y);
+    playerDamaging = true;
+  }
   
   // ゴールと接触した時
   gameClear(){
